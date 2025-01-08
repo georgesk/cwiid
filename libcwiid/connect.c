3c69fec6609051bf188c163b00b6f1cd567dbb9a
Georges Khaznadar <georgesk@debian.org>
2025-01-08 11:45:23 +0100

    added missin,g #include statements

--- libcwiid/connect.c
+++ libcwiid/connect.c
@@ -72,7 +72,10 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <stdint.h>
 #include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/hci_lib.h>
 #include <bluetooth/l2cap.h>
 #include "cwiid_internal.h"
 
